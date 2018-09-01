d753f907e5ac1accc2700e92dbaff5ddd775b78b
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2018-09-01 12:40:33 -0500

    widgets

--- HordeGame/Source/HordeGame/HordeGame.Build.cs
+++ HordeGame/Source/HordeGame/HordeGame.Build.cs
@@ -8,7 +8,7 @@ public class HordeGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
